4c04f8145bb660c59fbe74aa8a71299b6f8a2a98
lols <me_hotemail@hotmail.com>
2023-02-27 08:59:04 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ logging.basicConfig(
 
 ocpus = 4
 memory_in_gbs = 24
-wait_s_for_retry = 30
+wait_s_for_retry = 32
 
 logging.info("#####################################################")
 logging.info("Script to spawn VM.Standard.A1.Flex instance")
